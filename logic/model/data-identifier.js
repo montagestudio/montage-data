7bc775f624d7d5c8fa80df4f0105922a22064d90
Jonathan Miller <jonathan@konturlabs.com>
2017-01-25 08:17:26 -1000

    Refine documentation

--- logic/model/data-identifier.js
+++ logic/model/data-identifier.js
@@ -1,18 +1,25 @@
 var Montage = require("montage").Montage;
 
 /**
- * A DataIdentifier represents a universal identifier for an object managed by Montage Data.
- * It provides the support for uniquing in a DataService. Wether an object extists in
- * one or more local DataService in an Application or in a remote one, a DataIdentifier
- * encapsulate the information needed to uniquely identify an object, like a primary key in a database.
- * A DataIdentifier has a URL representation, which is conceptually aligned with the notion of resource:
- * It should have:
- * - a host/source/origin: where the data come from. Automatically generated primary keys
- *  exists in only one environment - Dev, test, prod, etc...,
- * (a user's authorization (if any necessary) should be left to be resolved a client receiving the identifier,
- * only people authenticated and authorized would be able to get it and that happens at DataService level)
+ * A DataIdentifier represents a universal identifier for an object managed by
+ * Montage Data.  It provides the support for uniquing in a DataService.
+ * Whether an object exists in one or more local DataServices in an Application
+ * or in a remote one, a DataIdentifier encapsulates the information needed to
+ * uniquely identify an object, like a primary key in a database.  A DataIdentifier
+ * has a URL representation, which is conceptually aligned with the notion of
+ * resource. It should have:
+ *
+ * - a host/source/origin: where the data come from. Automatically generated
+ * primary keys exists in only one environment - Dev, test, prod, etc...,
+ * (a user's authorization (if any necessary) should be left to be resolved
+ * by a client receiving the identifier, only people authenticated and authorized
+ * would be able to get it and that happens at DataService level)
+ *
  * - a type
- * - a primary key. This could be a combination of property/value, but it needs to be serializable as a valid url
+ *
+ * - a primary key. This could be a combination of property/value, but it needs
+ * to be serializable as a valid url
+ *
  * Exact details are not exposed and may vary per specific DataService or RawDataService
  *
  * @class
@@ -30,7 +37,7 @@ exports.DataIdentifier = Montage.specialize(/** @lends DataIdentifier.prototype 
     },
 
     /**
-     * The ObjectDescriptor associated witH a dataIdentifier
+     * The ObjectDescriptor associated with a dataIdentifier
      *
      * @type {ObjectDescriptor}
      */
@@ -39,36 +46,33 @@ exports.DataIdentifier = Montage.specialize(/** @lends DataIdentifier.prototype 
     },
 
     /**
-     * Wether a DataIdentifier is persistent/final vs temporary when created client side
+     * Whether a DataIdentifier is persistent/final vs temporary when created
+     * client side.
      *
-     * @type {ObjectDescriptor}
+     * @type {boolean}
      */
     isPersistent: {
         value: false
     },
 
-    /**
-     * Wether a DataIdentifier is persistent/final vs temporary when created client side
-     *
-     * @type {ObjectDescriptor}
-     */
     _identifier: {
         value: false
     },
 
     _url: {
-        value: false
+        value: undefined
     },
-  /**
+
+    /**
      * The url representation of a dataIdentifier
      *
-     * @type {ObjectDescriptor}
+     * @type {string}
      */
     url: {
-        get: function() {
+        get: function () {
             return this._url;
         },
-        set: function(value) {
+        set: function (value) {
             return this._url = value;
         }
     }
