d19dba22061c7a25f577c72ff0969bdd983f757a
Harold Thetiot <hthetiot@gmail.com>
2017-03-15 12:08:53 -0700

    update specs

--- test/run-node.js
+++ test/run-node.js
@@ -29,9 +29,8 @@ jasmineEnv.addReporter(consoleReporter);
 // Execute
 var mrRequire = require('mr/bootstrap-node');
 var PATH = require("path");
+
 mrRequire.loadPackage(PATH.join(__dirname, ".")).then(function (mr) {
-    return mr.async("run");
-}).then(function () {
-	console.log('Done');
+    return mr.async("all");
 }).thenReturn();
 
