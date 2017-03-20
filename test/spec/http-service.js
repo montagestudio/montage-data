0ff74b91f0b170d5254294f7428d62bd5c1bc4d2
Harold Thetiot <hthetiot@gmail.com>
2017-03-20 14:35:55 -0700

    update http-service spec

--- test/spec/http-service.js
+++ test/spec/http-service.js
@@ -1,24 +1,29 @@
 
 var DataService = require("montage-data/logic/service/data-service").DataService,
-    DataSelector = require("montage-data/logic/service/data-selector").DataSelector,
-    WeatherReport = require("spec/logic/model/weather-report").WeatherReport,
-    WeatherService = require("spec/logic/service/weather-service").WeatherService;
+    HttpService = require("montage-data/logic/service/http-service").HttpService,
+    DataSelector = require("montage-data/logic/service/data-selector").DataSelector
+    Criteria = require("montage/core/criteria").Criteria,
+    WeatherReport = require("./logic/model/weather-report").WeatherReport,
+    WeatherService = require("./logic/service/weather-service").WeatherService,
+    MontageSerializer = require("montage/core/serialization/serializer/montage-serializer").MontageSerializer;
 
 describe("An HttpService", function() {
 
     it("needs to be tested", function (done) {
 
-    	var criteria = {
+        var dataExpression = "city = $city && unit = $unit && country = $country";
+        var dataParameters = {
             city: 'San-Francisco',
             country: 'us',
             unit: 'imperial'
         };
+        var dataCriteria = new Criteria().initWithExpression(dataExpression, dataParameters);
+        var dataType = WeatherReport.TYPE;
+        var dataQuery  = DataSelector.withTypeAndCriteria(dataType, dataCriteria);
 
         var mainService = new DataService();
         mainService.addChildService(new WeatherService());
-    	
-    	var stream  = DataSelector.withTypeAndCriteria(WeatherReport.TYPE, criteria);
-        mainService.fetchData(stream).then(function (weatherReports) {
+        mainService.fetchData(dataQuery).then(function (weatherReports) {
             expect(typeof weatherReports[0].temp).toBe('number');
             done();
         });
