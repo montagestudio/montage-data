7c370b41903b1ef4a97767abcaf1b633b20dfa76
Harold Thetiot <hthetiot@gmail.com>
2017-03-20 09:56:52 -0700

    update http-service spec

--- test/spec/http-service.js
+++ test/spec/http-service.js
@@ -1,7 +1,27 @@
-var HttpService = require("montage-data/logic/service/http-service").HttpService;
+
+var DataService = require("montage-data/logic/service/data-service").DataService,
+    DataSelector = require("montage-data/logic/service/data-selector").DataSelector,
+    WeatherReport = require("spec/logic/model/weather-report").WeatherReport,
+    WeatherService = require("spec/logic/service/weather-service").WeatherService;
 
 describe("An HttpService", function() {
 
-    xit("needs to be tested", function () {});
+    it("needs to be tested", function (done) {
+
+    	var criteria = {
+            city: 'San-Francisco',
+            country: 'us',
+            unit: 'imperial'
+        };
+
+        var mainService = new DataService();
+        mainService.addChildService(new WeatherService());
+    	
+    	var stream  = DataSelector.withTypeAndCriteria(WeatherReport.TYPE, criteria);
+        mainService.fetchData(stream).then(function (weatherReports) {
+            expect(typeof weatherReports[0].temp).toBe('number');
+            done();
+        });
+    });
 
 });
